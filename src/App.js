ab541c88a0c18f59e3ab0c38feef799112905814
itsrealkaran <karansingh@duck.com>
2024-03-08 23:26:18 +0530

    Project Started

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Wait for the <strong>Banger</strong> to drop !!!
+        </p>
+        <a
+          className="App-link"
+          href="https://linkedin.com/in/itsrealkaran"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Itsrealkaran
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
